2f1eaa535230eedfd7dc9b99e0901da0c971cac8
Leon Noel <leon.a.noel+github@gmail.com>
2019-12-04 22:36:26 -0500

    Fully working Quickpound

---
+++ app/routes.js
@@ -0,0 +1,129 @@
+module.exports = function(app, passport, db, multer, ObjectId) {
+
+// Image Upload Code =========================================================================
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'public/images/uploads')
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.fieldname + '-' + Date.now() + ".png")
+    }
+});
+var upload = multer({storage: storage});
+
+
+// normal routes ===============================================================
+
+// show the home page (will also have our login links)
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+});
+
+// PROFILE SECTION =========================
+app.get('/profile', isLoggedIn, function(req, res) {
+    let uId = ObjectId(req.session.passport.user)
+    db.collection('posts').find({'posterId': uId}).toArray((err, result) => {
+      if (err) return console.log(err)
+      res.render('profile.ejs', {
+        user : req.user,
+        posts: result
+      })
+    })
+});
+
+// FEED PAGE =========================
+app.get('/feed', function(req, res) {
+    db.collection('posts').find().toArray((err, result) => {
+      if (err) return console.log(err)
+      res.render('feed.ejs', {
+        user : req.user,
+        posts: result
+      })
+    })
+});
+
+// INDIVIDUAL POST PAGE =========================
+app.get('/post/:zebra', function(req, res) {
+    let postId = ObjectId(req.params.zebra)
+    console.log(postId);
+    db.collection('posts').find({_id: postId}).toArray((err, result) => {
+      if (err) return console.log(err)
+      res.render('post.ejs', {
+        posts: result
+      })
+    })
+});
+
+//Create Post =========================================================================
+app.post('/qpPost', upload.single('file-to-upload'), (req, res, next) => {
+  let uId = ObjectId(req.session.passport.user)
+  db.collection('posts').save({posterId: uId, caption: req.body.caption, likes: 0, imgPath: 'images/uploads/' + req.file.filename}, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/profile')
+  })
+});
+
+// LOGOUT ==============================
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+// =============================================================================
+// AUTHENTICATE (FIRST LOGIN) ==================================================
+// =============================================================================
+
+    // locally --------------------------------
+        // LOGIN ===============================
+        // show the login form
+        app.get('/login', function(req, res) {
+            res.render('login.ejs', { message: req.flash('loginMessage') });
+        });
+
+        // process the login form
+        app.post('/login', passport.authenticate('local-login', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/login', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+        }));
+
+        // SIGNUP =================================
+        // show the signup form
+        app.get('/signup', function(req, res) {
+            res.render('signup.ejs', { message: req.flash('signupMessage') });
+        });
+
+        // process the signup form
+        app.post('/signup', passport.authenticate('local-signup', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/signup', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+        }));
+
+// =============================================================================
+// UNLINK ACCOUNTS =============================================================
+// =============================================================================
+// used to unlink accounts. for social accounts, just remove the token
+// for local account, remove email and password
+// user account will stay active in case they want to reconnect in the future
+
+    // local -----------------------------------
+    app.get('/unlink/local', isLoggedIn, function(req, res) {
+        var user            = req.user;
+        user.local.email    = undefined;
+        user.local.password = undefined;
+        user.save(function(err) {
+            res.redirect('/profile');
+        });
+    });
+
+};
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.redirect('/');
+}
---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://demo:demo@cluster0-92dvh.mongodb.net/quickpound?retryWrites=true&w=majority'
+// looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
